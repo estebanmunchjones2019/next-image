7be21a5a585e257905169684849e0cb178847e46
Esteban Munch Jones <esteban.munch.jones@gmail.com>
2021-06-10 11:49:36 +0100

    fix class

--- pages/index.js
+++ pages/index.js
@@ -57,11 +57,11 @@ export default function Home() {
               width={2400}
               height={1598}
               layout="responsive"
-              class="card-img-top" alt="..." />
+              className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
               </div>
             </div>
           </div>
